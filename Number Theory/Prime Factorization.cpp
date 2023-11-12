b4825bb1deced9ca9078b5a11360b8b7842a2976
Azman-Ahmed <86096683+Azman-Ahmed@users.noreply.github.com>
2023-11-13 03:06:24 +0600

    Prime Factorization.cpp

--- Number Theory/Prime Factorization.cpp
+++ Number Theory/Prime Factorization.cpp
@@ -10,6 +10,8 @@ int main()
 
     int n;
     cin>>n;
+
+    ///O(n)
     vector<int> prime_factors;
     for(int i=2; i<n; i++)
     {
@@ -20,4 +22,40 @@ int main()
         }
     }
     if(n>1) prime_factors.push_back(n);
+
+
+    ///O(sqrt(n))
+    for(int i=2; i*i<n; i++)
+    {
+        while(n%i==0)
+        {
+            prime_factors.push_back(i);
+            n/=i;
+        }
+    }
+    if(n>1) prime_factors.push_back(n);
+
+    for(int i=0; i<prime_factors.size(); i++) cout<<prime_factors[i]<<endl;
+
+    
+    /// O(nlogn) using LPF
+    for(int i=2; i<N; i++)
+    {
+        if(lpf[i]==0)
+        {
+            lpf[i] = i;
+            for(int j = 2*i; j<N; j+=i)
+            {
+                if(lpf[j]==0) lpf[j] = i;
+            }
+        }
+    }
+    cin>>n;
+    while(n > 1)
+    {
+        cout << lpf[n] << endl;
+        n/=lpf[n];
+    }
+    
+
 }
